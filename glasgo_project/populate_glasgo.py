a22fa26a14eafc12c0895f0a92dd7b6ad707993d
Ruaridh Wilson <RuaridhSWilson@gmail.com>
2020-03-02 10:46:54 +0000

    refactored population script
    and added kelvingrove art gallery and museum

--- glasgo_project/populate_glasgo.py
+++ glasgo_project/populate_glasgo.py
@@ -16,25 +16,63 @@ from glasgo_project import settings
 
 TEMP_DIR = os.path.join(settings.MEDIA_DIR, "temp")
 
+
+tags = {
+    "Museum": [],
+    "Festival": [],
+    "Cultural Landmark": [],
+    "Natural Landmark": [],
+    "Park": [],
+    "Sports": [],
+    "Monument": [],
+    "Free": ["Cheap", "Moderate", "Expensive"],
+    "Cheap": ["Free", "Moderate", "Expensive"],
+    "Moderate": ["Free", "Cheap", "Expensive"],
+    "Expensive": ["Free", "Cheap", "Moderate"],
+    "Family-friendly": [],
+    "Disabled Access": [],
+    "Parking Availability": [],
+}
+
+attractions = {
+    "Celtic Park Stadium": {
+        "link": "http://www.celticfc.net/mainindex",
+        "image": "https://cdn.getyourguide.com/img/tour_img-1222909-148.jpg",
+        "description": "Celtic Park in the Parkhead area of Glasgow, Scotland, is the home ground of Celtic Football Club. With a capacity of 60,411, it is the largest football stadium in Scotland, and the eighth-largest stadium in the United Kingdom. It is known by Celtic fans as Parkhead or Paradise.",
+        "location": "Celtic Park Stadium, The Celtic Way, Parkhead, Glasgow, G40 3RE",
+        "price_range": "MO",
+        "family_friendly": True,
+        "disabled_access": True,
+        "parking": True,
+        "multi_language": False,
+        "tags": ["Sports", "Cultural Landmark",],
+    },
+    "George Square": {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/a/ad/City_Chambers%2C_George_Square%2C_Glasgow_05.jpg",
+        "description": "George Square is the principal civic square in the city of Glasgow, Scotland. It is one of six squares in the city centre, the others being Cathedral Square, St Andrew's Square, St Enoch Square, Royal Exchange Square, and Blythswood Square on Blythswood Hill.",
+        "location": "George Square, Glasgow, G2 1DH",
+        "price_range": "FR",
+        "family_friendly": True,
+        "disabled_access": True,
+        "parking": False,
+        "multi_language": False,
+        "tags": ["Cultural Landmark", "Monument",],
+    },
+    "Kelvingrove Art Gallery and Museum": {
+        "image": "https://peoplemakeglasgow.com/images/Things_to_do/Museums_and_galleries/Kelvingrove-995.jpg",
+        "description": "Kelvingrove Art Gallery and Museum is a museum and art gallery in Glasgow, Scotland. It reopened in 2006 after a three-year refurbishment and since then has been one of Scotland's most popular visitor attractions. The museum has 22 galleries, housing a range of exhibits, including Renaissance art, taxidermy, and artifacts from ancient Egypt.",
+        "location": "Kelvingrove Art Gallery and Museum, Argyle St, Glasgow, G3 8AG",
+        "price_range": "FR",
+        "family_friendly": True,
+        "disabled_access": True,
+        "parking": False,
+        "multi-language": True,
+        "tags": ["Cultural Landmark", "Museum",]
+    },
+}
+
+
 def populate_tags():
-
-    tags = {
-        "Museum": [],
-        "Festival": [],
-        "Cultural Landmark": [],
-        "Natural Landmark": [],
-        "Park": [],
-        "Sports": [],
-        "Monument": [],
-        "Free": ["Cheap", "Moderate", "Expensive"],
-        "Cheap": ["Free", "Moderate", "Expensive"],
-        "Moderate": ["Free", "Cheap", "Expensive"],
-        "Expensive": ["Free", "Cheap", "Moderate"],
-        "Family-friendly": [],
-        "Disabled Access": [],
-        "Parking Availability": [],
-    }
-
     for tag in tags.keys():
         add_tag(tag)
     for tag, mutex_tags in tags.items():
@@ -42,32 +80,6 @@ def populate_tags():
 
 
 def populate_attractions():
-    attractions = {
-        "Celtic Park Stadium": {
-            "link": "http://www.celticfc.net/mainindex",
-            "image": "https://cdn.getyourguide.com/img/tour_img-1222909-148.jpg",
-            "description": "Celtic Park in the Parkhead area of Glasgow, Scotland, is the home ground of Celtic Football Club. With a capacity of 60,411, it is the largest football stadium in Scotland, and the eighth-largest stadium in the United Kingdom. It is known by Celtic fans as Parkhead or Paradise.",
-            "location": "Celtic Park Stadium, The Celtic Way, Parkhead, Glasgow, G40 3RE",
-            "price_range": "MO",
-            "family_friendly": True,
-            "disabled_access": True,
-            "parking": True,
-            "multi_language": False,
-            "tags": ["Sports", "Cultural Landmark",],
-        },
-        "George Square": {
-            "image": "https://upload.wikimedia.org/wikipedia/commons/a/ad/City_Chambers%2C_George_Square%2C_Glasgow_05.jpg",
-            "description": "George Square is the principal civic square in the city of Glasgow, Scotland. It is one of six squares in the city centre, the others being Cathedral Square, St Andrew's Square, St Enoch Square, Royal Exchange Square, and Blythswood Square on Blythswood Hill.",
-            "location": "George Square, Glasgow, G2 1DH",
-            "price_range": "FR",
-            "family_friendly": True,
-            "disabled_access": True,
-            "parking": False,
-            "multi_language": False,
-            "tags": ["Cultural Landmark", "Monument",],
-        },
-    }
-
     for attraction, kwargs in attractions.items():
         add_attraction(attraction, **kwargs)
 
