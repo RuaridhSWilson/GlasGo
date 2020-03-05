839ccab283c80dfedbe33d18c664225429383f1c
Joel <jfev20@googlemail.com>
2020-03-05 17:41:11 +0000

    Implemented time since added
    -add time since added display for attractions on homepage
    -changed the added attribute to make use of signals rather than auto_now_add

--- glasgo_project/glasgo/signals.py
+++ glasgo_project/glasgo/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from datetime import datetime
 
-from glasgo.models import Vote
+from glasgo.models import Vote, Attraction
 
 
 @receiver(post_save, sender=Vote)
@@ -12,3 +13,10 @@ def save_vote(sender, instance, created, **kwargs):
         else:
             instance.attraction.rating -= 1
         instance.attraction.save()
+
+
+@receiver(post_save, sender=Attraction)
+def save_added_time(sender, instance, created, **kwargs):
+    if created:
+        instance.added = datetime.now()
+        instance.save()
