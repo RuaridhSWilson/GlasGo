e84a7bb63ff5036e604fa628420603b8020e6715
Ruaridh Wilson <RuaridhSWilson@gmail.com>
2020-03-05 03:25:28 +0000

    styling and layout for sidebar and categories listing
    + added navbar padding javascript
    + added stylesheet with thumbnail classes
    * updated attractions listing
    * updated sidebar in base template
    * removed redundant tags in base template
    + added attractions listing to home page
    * added a bottom margin to the search box
    * updated top ten list

--- glasgo_project/static/js/glasgo-query.js
+++ glasgo_project/static/js/glasgo-query.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
 
+    function pad_navbar() {
+        $("body").css("padding-top", $("#top-navbar").height()+10);
+    }
+    $(window).resize(pad_navbar());
+    $(window).load(pad_navbar()); // TODO: Figure out why this is causing a TypeError
 });
