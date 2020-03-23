b8ca36d5dc87fb02c1673c73df69bb87aa410c9a
Torrin McQueen <2378609m@student.gla.ac.uk>
2020-03-23 14:50:47 +0000

    Changed search back to order by recently added

--- glasgo_project/glasgo/helper_functions.py
+++ glasgo_project/glasgo/helper_functions.py
@@ -5,7 +5,7 @@ from glasgo.models import Attraction, Tag
 
 def get_attractions(contains="", tags=None):
     if tags:
-        attractions = [Tag.objects.get(slug=tag).attraction_set.all() for tag in tags]
+        attractions = [Tag.objects.get(slug=tag).attraction_set for tag in tags]
         attractions = attractions[0].intersection(*attractions[1:]).filter(approved=True)
     else:
         attractions = Attraction.objects.filter(approved=True)
@@ -13,4 +13,4 @@ def get_attractions(contains="", tags=None):
     if contains:
         attractions = attractions.intersection(Attraction.objects.filter(title__icontains=contains, approved=True))
 
-    return attractions.order_by("-rating")
+    return attractions.order_by("-added")
