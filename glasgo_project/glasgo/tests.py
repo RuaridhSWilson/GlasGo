8f2ca0f6788a378cd8310e5b3d7dd460b4790a1d
Torrin McQueen <2378609m@student.gla.ac.uk>
2020-04-03 16:28:32 +0000

    Removed TwD header

--- glasgo_project/glasgo/tests.py
+++ glasgo_project/glasgo/tests.py
@@ -8,7 +8,7 @@ from glasgo.models import Attraction, Vote
 
 #from populate_glasgo import populate
 
-FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}TwD Test Failure =({os.linesep}================{os.linesep}"
+FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}Test Failure =({os.linesep}================{os.linesep}"
 FAILURE_FOOTER = f"{os.linesep}"
 
 class AboutPageTests(TestCase):
@@ -44,9 +44,9 @@ class AboutPageTests(TestCase):
         self.assertTrue(is_callable, f"{FAILURE_HEADER}Can't execute the about view{FAILURE_FOOTER}")
 
     def test_response(self):
-        
+
         response = self.client.get(reverse('glasgo:about'))
-        
+
         self.assertEqual(response.status_code, 200, f"{FAILURE_HEADER}When requesting the about view, the server did not respond correctly.{FAILURE_FOOTER}")
         self.assertContains(response, "About Us", msg_prefix=f"{FAILURE_HEADER}The about view did not respond with the expected message.{FAILURE_FOOTER}")
 
@@ -54,7 +54,7 @@ class AboutPageTests(TestCase):
         """
         checks to see if block in about page overrides the footer block
         """
-        
+
        # populate()
         template_base_path = os.path.join(settings.TEMPLATE_DIR, 'glasgo')
 
@@ -89,8 +89,8 @@ class HomePageTests(TestCase):
         response = self.client.get(reverse('glasgo:home'))
 
         self.assertEqual(response.status_code, 200, f"{FAILURE_HEADER}Home page requested is not same as expected{FAILURE_FOOTER}")
-        
-        #NEED TO CHECK IF HOMEPAGE ACTUALLY DISPLAYS WHAT ITS SUPPOSED TO 
+
+        #NEED TO CHECK IF HOMEPAGE ACTUALLY DISPLAYS WHAT ITS SUPPOSED TO
 
 
 #class AttractionPageTests(TestCase):
@@ -120,7 +120,7 @@ class HomePageTests(TestCase):
     """
     Tests if it is visible in every page, have heading "Top 10 Attractions", be solved by top 10 votes, contain thumbnail, title and vote number
     """
-    
+
 #    def test_for_existance_of_top_10_attraction_sidebar(self):
 
 #class LoginPageTests(TestCase):
